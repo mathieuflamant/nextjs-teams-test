4f3552b3e368d12e35291a23cb673da841f8369b
Mat Flamant <mf>
2025-06-30 21:43:35 -0400

    Revert "Log environment variables during build"
    This reverts commit 2e50c13dda8de4055f65d448c593e838d8d8e9b2.

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,5 @@
 import type { NextConfig } from "next";
 
-// Log environment variables during build
-console.log("ENV VARS AT BUILD TIME:");
-for (const [key, value] of Object.entries(process.env)) {
-  console.log(`${key}=${value}`);
-}
-
 const nextConfig: NextConfig = {
   /* config options here */
 };
