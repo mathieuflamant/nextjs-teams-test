0c160f8b7235ae9add00dcbfa74ec68a684c6143
Mat Flamant <mf>
2025-07-03 17:07:48 -0400

    Add vars to next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,13 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  env: {
+    MICROSOFT_ISSUER: process.env.MICROSOFT_ISSUER,
+    COGNITO_TOKEN_ENDPOINT: process.env.COGNITO_TOKEN_ENDPOINT,
+    COGNITO_CLIENT_ID: process.env.COGNITO_CLIENT_ID,
+    COGNITO_CLIENT_SECRET: process.env.COGNITO_CLIENT_SECRET,
+    AZURE_CLIENT_SECRET: process.env.AZURE_CLIENT_SECRET,
+  },
 };
 
 export default nextConfig;
